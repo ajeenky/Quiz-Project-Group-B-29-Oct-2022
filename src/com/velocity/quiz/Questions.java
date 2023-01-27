2ce20b902a0c474ae649b323941b8b7210f87bc0
Akanksha <akankshakhair123456@gmail.com>
2023-01-27 11:17:41 -0800

    My part is Done

--- src/com/velocity/quiz/Questions.java
+++ src/com/velocity/quiz/Questions.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,33 +14,51 @@ public class Questions {
 //	1.	Store the 10 multiple choice questions related to java into database with correct answer.
 //	2.	Out of these, user should able to presented random questions.
 //	3.	One question has four options only.
-	ArrayList<String> al = new ArrayList<String>();
 	
-	public void getQuestionsFromDB() {
-		try {
-			Scanner sc = new Scanner(System.in);
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/questions", "root", "root");
-			PreparedStatement pstmt = con.prepareStatement("select * from quiz order by rand()");
-			ResultSet res = pstmt.executeQuery();
-			while (res.next()) {
-				
-				System.out.println(res.getInt(1)+" "+res.getString(2));
-				System.out.println(res.getString(3)+"  "+res.getString(4)+"  "+res.getString(5)+"  "+res.getString(6));
-				System.out.println("Enter a option");
-				String input = sc.next();
-				al.add(input);
+	   UserInput user = new UserInput();
+		ArrayList<String> al = new ArrayList<String>();
+		ArrayList<String> list = new ArrayList<String>();
+		int result;
+		 String  grade; 
+		 String grace;
+		public void getQuestionsFromDB() {
+			try {
+				Scanner sc = new Scanner(System.in);
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/questions", "root", "root");
+				PreparedStatement pstmt = con.prepareStatement("select * from quiz order by rand()");
+				ResultSet res = pstmt.executeQuery();
+				while (res.next()) {
+						System.out.println(res.getInt(1)+" "+res.getString(2));
+						System.out.println(res.getString(3)+"  "+res.getString(4)+"  "+res.getString(5)+"  "+res.getString(6));
+						res.getString(7);
+						System.out.println("Enter a option");
+						String input = sc.next();
+						String score =res.getString(7);
+						list.add(score);
+						al.add(input);
+						//Calculating the score
+						int count=0;
+				        Iterator itr = al.iterator();
+				        Iterator it = list.iterator();
+				        while(itr.hasNext()) {
+				         it.hasNext();
+				         if(itr.next().equals(it.next())) {
+				            count++;
+				           }
+				         }
+					//Calculating the grade
+			             result=count;
+			             
+				} 
+				  System.out.println("your score is :: "+ result);
+				  System.out.println("Your Grade is :: "+ null);
+				      
+				  con.close();
+				  pstmt.close();
+	
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			System.out.println(al);
-			con.close();
-			pstmt.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 	}
-	public static void main(String[] args) {
-		Questions questions = new Questions();
-		questions.getQuestionsFromDB();
-	}
-}
--- src/com/velocity/quiz/UserInput.java
+++ src/com/velocity/quiz/UserInput.java
@@ -1,8 +1,63 @@
 package com.velocity.quiz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class UserInput {
 // For Akanksha
-//	4.	User will choose the answers.
+//	4.	User will choose thAe answers.
+
 //	5.	Also ability to store the multiple student score into database.
 
+		int result;
+		Connection con = null;
+		PreparedStatement pst = null;	
+		public Connection getConnectiondetails(){
+			
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/questions","root","root");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return con;
+		}
+		public void getuserdetail(int id,String name,int score,String mark) {
+			
+			try {
+				getConnectiondetails();
+				pst = con.prepareStatement("insert into user_input(Id,Name,Score,Grade)values(?,?,?,?)");
+				pst.setInt(1, id);
+				pst.setString(2, name);
+				pst.setInt(3,score);
+	        	pst.setString(4, mark);
+				int i=pst.executeUpdate();
+				System.out.println("Record inset Successfully :: "+i);
+			  
+			} catch (Exception e) {
+			e.printStackTrace();	
+			}
+		}
+	   
+		public static void main(String []args) {
+		       
+				
+		        System.out.println(" *** Quize Started *** ");
+		        Questions questions = new Questions();
+		        questions.getQuestionsFromDB();
+		        Scanner sc = new Scanner(System.in);
+		        System.out.println("Enter you Id::");
+		        int id =sc.nextInt();
+		        System.out.println("Enter you Name::");
+		        String name =sc.next();
+		        int score =questions.result;
+		        String mark=questions.grade;
+		        UserInput user = new UserInput();
+		        user.getuserdetail( id,name,score, mark);
+}
 }
